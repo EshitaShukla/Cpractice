c6c33591fb8d64ce3e8fc78458c817aeddfc9bfd
Anirudh Tiwari <anirudh.tiwari@colorado.edu>
2018-02-05 01:24:21 -0700

    Adding a new node to the list with 3 nodes

---
+++ LinkedList/addNodeInBtw.c
@@ -0,0 +1,64 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+struct Node{
+  int data;
+  struct Node *next;
+};
+
+void printlist(struct Node* first){
+ while(first != NULL){
+   printf("Node Data: %d\n", first->data);
+   first = first->next;
+ }
+}
+
+// void pushNode(struct Node** head_ref, int new_data){
+
+//   struct Node* new_node = NULL;
+//   new_node = (struct Node*) malloc(sizeof(struct Node));
+
+//   new_node->data = new_data;
+//   new_node->next = (*head_ref);
+//   *head_ref = new_node;
+// }
+
+void pushBetweenNode(struct Node* prev_node, int new_data){
+  struct Node* new_node = (struct Node*)(malloc(sizeof(struct Node*)));
+  new_node->data = new_data;
+  new_node->next = prev_node->next;
+  prev_node->next = new_node;
+}
+int main(){
+  struct Node* head = NULL;
+  struct Node* second = NULL;
+  struct Node* third = NULL;
+
+  head = (struct Node*) malloc(sizeof(struct Node));
+  second = (struct Node*) malloc(sizeof(struct Node));
+  third = (struct Node*) malloc(sizeof(struct Node));
+
+  head->data = 1;
+  head->next = second;
+
+  second->data = 2;
+  second->next = third;
+
+  third->data = 3;
+  third->next = NULL;
+
+  printlist(head);
+
+  int new_data = 5;
+
+  //pushNode(&head, new_data);
+  pushBetweenNode(second, new_data);
+
+  printf("New List: \n");
+
+  printlist(head);
+
+
+  return 0;
+
+}
