2b2cc6cf49218a98df863c195bd57d1a38def98a
Anirudh Tiwari <anirudh.tiwari@colorado.edu>
2018-03-03 17:57:32 -0700

    Stack Implementation with array

--- Stack/stackArray.c
+++ Stack/stackArray.c
@@ -1,60 +1,74 @@
-// C program for linked list implementation of stack
-#include <stdio.h>
-#include <stdlib.h>
-#include <limits.h>
+#include<stdio.h>
+#include<stdlib.h>
+
 
 struct Stack{
-    unsigned capacity;
-    int top;
-    int *array;
+  int top;
+  unsigned capacity;
+  int* array;
 };
 
-int ifFull(struct Stack* stack){
-    if(stack->top == stack->capacity-1)
+int isFull(struct Stack* stack){
+  if(stack->top == stack->capacity-1){
     return 1;
+  }
 
-    else return 0;
+  return 0;
 }
 
 int isEmpty(struct Stack* stack){
-    if(stack->top == -1){
-        return 1;
-    }
-    else return 0;
-}
-
-int push(struct Stack* stack, unsigned data){
-    if(ifFull(stack)){
-        return -1;
-    }
-    stack->array[++stack->top] = data;
-    printf("Pushed data to stack: %d\n", stack->array[stack->top]);
+  if(stack->top == -1){
+    printf("Empty Stack\n");
     return 1;
+  }
+
+  return 0;
 }
 
-int pop(struct Stack* stack){
-    if(isEmpty(stack)){
-        return INT_MIN;
-    }
-    return stack->array[stack->top--];
 
+void pop(struct Stack* stack){
+  if(isEmpty(stack)){
+    printf("Underflow \n");
+    return;
+  }
+  stack->array[stack->top--];
+  if(stack->top > -1)
+  printf("The top element now is: %d\n", stack->array[stack->top]);
+
+  else printf("No data on stack\n");
+  return;
 }
+
+void push(struct Stack* stack, int data){
+  if(isFull(stack)){
+    printf("Overflow \n");
+    return;
+  }
+  stack->array[++stack->top] = data;
+
+  printf("The data pushed is: %d\n", stack->array[stack->top]);
+  return;
+}
+
 
 struct Stack* createStack(unsigned capacity){
-    struct Stack* stack = (struct Stack*) malloc(sizeof(struct Stack));
-    stack->capacity = capacity;
-    stack->top = -1;
-    stack->array = (int *) malloc((stack->capacity)* sizeof(int));
 
-    return stack;
+  struct Stack* stack = (struct Stack*) malloc(sizeof(struct Stack*));
+  stack->capacity = capacity;
+  stack->top = -1;
+  stack->array = (int*) malloc(capacity * sizeof(int));
+
+  return stack;
 }
 
-int main(){
-    struct Stack* stack = createStack(100);
-    push(stack, 10);
-    push(stack, 20);
+int main(void){
 
-    printf("Removed %d from stack\n", pop(stack)); //Remove one data at a time from the top
+  struct Stack* stack = createStack(100);
+  push(stack, 4);
+  push(stack, 9);
+  //push(stack, 16);
 
-    return 0;
+  pop(stack);
+  pop(stack);
+  return 0;
 }
