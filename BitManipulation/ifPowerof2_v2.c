1f153e251cee4988a84f2819a41ef912e76b20b6
Anirudh Tiwari <anirudh.tiwari@colorado.edu>
2018-04-16 11:29:54 -0600

    My method>>>To check weather the given number is power of 2

---
+++ BitManipulation/ifPowerof2_v2.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+
+int main(void){
+  int x = 11;
+  int one = 0;
+
+  if(x<1) return -1;
+
+  while(x){
+    x=x>>1;
+    if(x&1)one++;
+  }
+  if(one>1) printf("Not a power");
+  else printf("Is a power");
+}
